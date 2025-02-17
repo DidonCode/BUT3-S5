951c8dff085d3af7ac4125b49ff233c5084a6b3e
EbMian <127929560+EbMian@users.noreply.github.com>
2025-02-17 20:03:36 +0100

    Ajout d'animations page swipe

--- docker/web/pages/music/swipe.php
+++ docker/web/pages/music/swipe.php
@@ -1,12 +1,17 @@
-<div id="" class="mx-auto">
-    <h2>Viva la vida</h2>
-    <img src="/web/images/Tests/viva-la-vida.jpg" alt="Image de couverture musique Viva la vida">
-    <div>
-        <button class="btn mr-5">
+<div class="col-md-4"></div>
+<div id="" class="mx-auto col-12 col-md-4">
+    <h2 class="text-center mb-3">Viva la vida</h2>
+    <div class="text-center">
+        <img class="rounded-img" src="/web/images/Tests/viva-la-vida.jpg" alt="Image de couverture musique Viva la vida" id="first-song">
+        <img class="rounded-img secondSong" src="/web/images/Tests/viva-la-vida.jpg" alt="Image de couverture musique Viva la vida">
+    </div>
+    <div class="text-center mt-4">
+        <button class="btn mr-5" id="dislike-swipe">
             <i class="fa-solid fa-xmark fa-2xl" style="color:#808080"></i>
         </button>
-        <button class="btn like-swipe" style="background-color: #F7CBCB;">
+        <button class="btn like-swipe" style="background-color: #F7CBCB;" id="like-swipe">
             <i class="fa-solid fa-heart fa-xl" style="color:#E80000"></i>
         </button>
     </div>
 </div>
+<div></div>
