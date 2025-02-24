a80adf0f0f35736fef24f023fcc990a0ca7fb6f3
Yanom <65492446+Yan0m@users.noreply.github.com>
2025-02-24 14:49:17 +0100

    Swipe : Ajout de génération de cards ainsi que leur suppression

--- docker/web/js/pages/music/swipe.js
+++ docker/web/js/pages/music/swipe.js
@@ -1,28 +1,28 @@
 (() => {
-    const contentContainer = document.getElementById("content-container");
-    const cardContainer = document.getElementById("card-container");
+	const contentContainer = document.getElementById('content-container');
+	const cardContainer = document.getElementById('card-container');
 
 	const dislikeBtn = document.getElementById('dislike-swipe');
 	const likeBtn = document.getElementById('like-swipe');
 
-    function createCard(card, i) {
-        sound = document.createElement("img");
-        sound.classList.add("rounded-img", "firstSong");
+	function createCard(card, i) {
+		sound = document.createElement('img');
+		sound.classList.add('rounded-img', 'firstSong');
 
-        if(i < 0) {
-            sound.classList.add("secondSong")
-        }
-        
-        sound.alt= `Image de couverture de musique : ${card.title} `;
-        sound.src = card.image
-        sound.style.zIndex = i;
+		if (i < 0) {
+			sound.classList.add('secondSong');
+		}
 
-        cardTitle = document.createElement("h2");
-        cardTitle.classList.add("text-center", "mb-3");
-        cardTitle.innerText = card.title
+		sound.alt = `Image de couverture de musique : ${card.title} `;
+		sound.src = card.image;
+		sound.style.zIndex = i;
 
-        cardContainer.appendChild(sound);
-    }
+		cardTitle = document.createElement('h2');
+		cardTitle.classList.add('text-center', 'mb-3');
+		cardTitle.innerText = card.title;
+
+		cardContainer.appendChild(sound);
+	}
 
 	/*
     async function sounds() {
@@ -53,88 +53,86 @@
     }
         */
 
-    const test = 
-    [
-        {
-            "id" : "1",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/1.jpg",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-        {
-            "id" : "5",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/5.png",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-        {
-            "id" : "20",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/20.jpg",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-        {
-            "id" : "27",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/27.png",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-        {
-            "id" : "28",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/28.jpg",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-        {
-            "id" : "30",
-            "title" : "Test",
-            "artist": "1",
-            "type" : "1",
-            "image" : "http://localhost:8081/storage/sound/image/30.png",
-            "link" : "http://localhost:8081/storage/sound/file/1.mp3",
-        },
-    ]
+	const test = [
+		{
+			id: '1',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/1.jpg',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+		{
+			id: '5',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/5.png',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+		{
+			id: '20',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/20.jpg',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+		{
+			id: '27',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/27.png',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+		{
+			id: '28',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/28.jpg',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+		{
+			id: '30',
+			title: 'Test',
+			artist: '1',
+			type: '1',
+			image: 'http://localhost:8081/storage/sound/image/30.png',
+			link: 'http://localhost:8081/storage/sound/file/1.mp3',
+		},
+	];
 
-    function generateCards(test) {
-        let i = 0;
-        for(card of test) {
-            createCard(card, i)
-            i--
-        }
-    }
+	function generateCards(test) {
+		let i = 0;
+		for (card of test) {
+			createCard(card, i);
+			i--;
+		}
+	}
 
-	const remove = async(isLiked) => {
-        if(cardContainer.children.length > 0) {
-            const firstCard = cardContainer.children[0];
+	const remove = async (isLiked) => {
+		if (cardContainer.children.length > 0) {
+			const firstCard = cardContainer.children[0];
 
-		    if (isLiked) {
-			    firstCard.classList.add('liked-song');
-		    } else {
-			    firstCard.classList.add('unliked-song');
-		    }
+			if (isLiked) {
+				firstCard.classList.add('liked-song');
+			} else {
+				firstCard.classList.add('unliked-song');
+			}
 
-            await new Promise((resolve) => {
-                firstCard.addEventListener('animationend', resolve, { once: true});
-            });
+			await new Promise((resolve) => {
+				firstCard.addEventListener('animationend', resolve, { once: true });
+			});
 
-            firstCard.remove();
-            if(cardContainer.children.length > 0) {
-                cardContainer.children[0].id = 'first-song';
+			firstCard.remove();
+			if (cardContainer.children.length > 0) {
+				cardContainer.children[0].id = 'first-song';
 
-                cardContainer.children[0].classList.remove("secondSong");
-            }
-                        
-        }    
+				cardContainer.children[0].classList.remove('secondSong');
+			}
+		}
 	};
 
 	dislikeBtn.onclick = function () {
@@ -145,5 +143,5 @@
 		remove(1);
 	};
 
-    generateCards(test);
+	generateCards(test);
 })();
