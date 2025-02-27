3f5c21269c53f3836cc02f51c5396f531ed6b0b5
EbMian <127929560+EbMian@users.noreply.github.com>
2025-02-27 22:29:56 +0100

    Correction erreur sur l'ouverture de la popup de signalement

--- docker/web/js/widgets/report_popup.js
+++ docker/web/js/widgets/report_popup.js
@@ -3,28 +3,29 @@
 		const reportingPopup = document.getElementById('reporting-popup');
 		const closePopup = document.getElementById('close-popup');
 		const openPopupBtn = document.getElementById('open-popup');
-		console.log('button', openPopupBtn);
 		const otherChoice = document.getElementById('other-choice');
 		const reportComment = document.getElementById('report-comment');
-
-		openPopupBtn.onclick = function () {
-			reportingPopup.style.display = 'block';
-		};
-
-		closePopup.onclick = function () {
-			hidePopup();
-		};
-
-		function hidePopup() {
-			reportingPopup.style.display = 'none';
-		}
-
-		otherChoice.onclick = function () {
-			if (otherChoice.checked) {
-				reportComment.removeAttribute('hidden');
-			} else {
-				reportComment.setAttribute('hidden', true);
+		
+		if (openPopupBtn != null) {
+			openPopupBtn.onclick = function () {
+				reportingPopup.style.display = 'block';
+			};
+	
+			closePopup.onclick = function () {
+				hidePopup();
+			};
+	
+			function hidePopup() {
+				reportingPopup.style.display = 'none';
 			}
-		};
+	
+			otherChoice.onclick = function () {
+				if (otherChoice.checked) {
+					reportComment.removeAttribute('hidden');
+				} else {
+					reportComment.setAttribute('hidden', true);
+				}
+			};
+		}
 	};
 })();
