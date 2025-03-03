2b7c2fd709be9405d6f80d6c9e80009f6ed01699
EbMian <127929560+EbMian@users.noreply.github.com>
2025-03-03 21:25:38 +0100

    Modification bouton requis pour ouverture popup de signalement

--- docker/web/js/widgets/report_popup.js
+++ docker/web/js/widgets/report_popup.js
@@ -1,31 +1,37 @@
 (() => {
 	window.onload = function () {
 		const reportingPopup = document.getElementById('reporting-popup');
-		const closePopup = document.getElementById('close-popup');
-		const openPopupBtn = document.getElementById('open-popup');
+		const closePopup = document.getElementById('close-reporting-popup');
+		const openPopupBtns = document.getElementsByClassName('open-reporting-popup');
 		const otherChoice = document.getElementById('other-choice');
 		const reportComment = document.getElementById('report-comment');
 
-		if (openPopupBtn != null) {
-			openPopupBtn.onclick = function () {
-				reportingPopup.style.display = 'block';
-			};
+		if (openPopupBtns != null) {
+			for (let i = 0; i < openPopupBtns.length; i++) {
+				openPopupBtns[i].addEventListener('click', showPopup, false);
+			}
 
 			closePopup.onclick = function () {
 				hidePopup();
 			};
 
-			function hidePopup() {
-				reportingPopup.style.display = 'none';
-			}
-
-			otherChoice.onclick = function () {
-				if (otherChoice.checked) {
-					reportComment.removeAttribute('hidden');
-				} else {
-					reportComment.setAttribute('hidden', true);
-				}
-			};
 		}
-	};
+
+		otherChoice.onclick = function () {
+			if (otherChoice.checked) {
+				reportComment.removeAttribute('hidden');
+			} else {
+				reportComment.setAttribute('hidden', true);
+			}
+		};
+	
+		function hidePopup() {
+			reportingPopup.style.display = 'none';
+		}
+		function showPopup() {
+			reportingPopup.style.display = 'block';
+		}
+	}
+
+	
 })();
